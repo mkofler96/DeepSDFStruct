63387ce8c3a3a4168f52366421f663ce88d6e249
Michael Kofler <michael.kofler@tuwien.ac.at>
2025-09-17 10:49:58 +0200

    added documentation

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+from importlib.metadata import version as package_version
+
+project = "DeepSDFStruct"
+copyright = "2025, Michael Kofler"
+author = "Michael Kofler"
+release = str(package_version("DeepSDFStruct"))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+
+# -- Extensions --------------------------------------------------------------
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+]
+
+
+autosummary_context = {"skipmethods": ["__init__"]}
+
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "autosummary": True,
+}
+
+html_theme_options = {"collapse_navigation": False, "navigation_depth": 4}
