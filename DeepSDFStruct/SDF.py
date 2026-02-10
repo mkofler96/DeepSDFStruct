fixed bounds scaling for deepLS reconstruction

DeepSDFStruct/SDF.py
@@ -687,7 +687,7 @@ class SDFfromDeepSDF(SDFBase):
     def _compute(self, queries: torch.Tensor) -> torch.Tensor:
         # DeepSDF queries range from -1 to 1
         orig_device = queries.device
-        queries = queries.to(self.get_device()) * 2 - 1
+        queries = queries.to(self.get_device())
         n_queries = queries.shape[0]
 
         sdf_values = torch.zeros(n_queries, device=self.get_device())

DeepSDFStruct/deep_sdf/reconstruction.py
@@ -5,6 +5,7 @@ from DeepSDFStruct.sampling import SampledSDF
 from DeepSDFStruct.deep_sdf.plotting import plot_reconstruction_loss
 from tqdm import trange
 import torch
+import torch.nn as nn
 
 
 def reconstruct_from_samples(
@@ -97,10 +98,10 @@ def reconstruct_from_samples(
             loss_history, iters_per_epoch=len(dataloader), filename=loss_plot_path
         )
 
-        params = list(sdf.parameters())
-        print(params)
+    params = list(sdf.parameters())
+    print(params)
 
-        return params
+    return params
 
 
 def reconstruct_deepLS_from_samples(
@@ -113,11 +114,15 @@ def reconstruct_deepLS_from_samples(
     loss_fn="ClampedL1",
     batch_size=512,
     drop_last=True,
+    loss_plot_path=None,
+    use_tanh_on_gt=False,
 ):
 
     optimizer = torch.optim.Adam(sdf.parameters(), lr=lr)
 
     gt_dist = sdfSample.distances
+    if use_tanh_on_gt:
+        gt_dist = nn.Tanh()(gt_dist)
 
     pbar = trange(num_iterations, desc="Reconstructing SDF from mesh", leave=True)
 
@@ -143,6 +148,7 @@ def reconstruct_deepLS_from_samples(
         dataset, batch_size=batch_size, shuffle=True, drop_last=drop_last
     )
 
+    loss_history = []
     for e in pbar:
         for querie_batch, gt_batch in dataloader:
             optimizer.zero_grad()
@@ -152,8 +158,16 @@ def reconstruct_deepLS_from_samples(
             optimizer.step()
             loss_num = loss.detach().item()
             pbar.set_postfix({"loss": f"{loss_num:.5f}"})
+            loss_history.append(loss_num)
 
     print("Reconstructed parameters:")
     params = list(sdf.parameters())
     print(params)
+    if loss_plot_path is not None:
+        plot_reconstruction_loss(
+            loss_history, iters_per_epoch=len(dataloader), filename=loss_plot_path
+        )
+
+    params = list(sdf.parameters())
+
     return params

DeepSDFStruct/flexicubes/flexicubes.py
@@ -189,10 +189,7 @@ class FlexiCubes:
             bounds = torch.as_tensor(
                 bounds, device=verts_unique.device, dtype=verts_unique.dtype
             )
-            assert bounds.shape == (
-                2,
-                3,
-            ), f"bounds must have shape [2, 3] but have shape {bounds.shape}"
+            assert bounds.shape == (2, 3), "bounds must have shape [2, 3]"
 
         # Scale samples from [0, 1] to the given bounds
         # samples = samples * 1.1 + _torch.tensor([0.5, 0.5, 0.5], device=device)

DeepSDFStruct/lattice_structure.py
@@ -121,6 +121,7 @@ class LatticeSDFStruct(_SDFBase):
         deformation_spline: TorchSpline | None = None,
         microtile: _SDFBase | None = None,
         parametrization: _torch.nn.Module | None = None,
+        bounds=None,
     ):
         """Helper class to facilitate the construction of microstructures.
 
@@ -144,6 +145,12 @@ class LatticeSDFStruct(_SDFBase):
         self.tiling = tiling
         self.microtile = microtile
         self.geometric_dim = len(tiling)
+        if bounds is not None:
+            self.bounds = bounds
+        elif bounds is None and deformation_spline is not None:
+            self.bounds = deformation_spline.spline.parametric_bounds
+        else:
+            self.bounds = _np.array([[-1, 1]] * self.geometric_dim)
 
     @property
     def parametric_dimension(self):
@@ -179,21 +186,20 @@ class LatticeSDFStruct(_SDFBase):
         """
         orig_device = samples.device
         orig_dtype = samples.dtype
-        bounds = _torch.tensor(
-            self.deformation_spline.spline.parametric_bounds,
-            device=orig_device,
-            dtype=orig_dtype,
-        )
+        bounds = _torch.tensor(self.bounds, device=orig_device, dtype=orig_dtype)
         if self.parametrization is not None:
-            spline_domain_samples = _torch.clamp(samples, min=bounds[0], max=bounds[1])
-            parameters = self.parametrization(spline_domain_samples)
+            samples_parameter_space = (samples - bounds[0]) / (bounds[1] - bounds[0])
+            samples_parameter_space = _torch.clamp(samples_parameter_space, 0.0, 1.0)
+            parameters = self.parametrization(samples_parameter_space)
             self.microtile._set_param(parameters)
 
         queries_transformed = _torch.zeros_like(samples)
         for i_dim, t in enumerate(self.tiling):
-            queries_transformed[:, i_dim] = transform(samples[:, i_dim], t)
+            queries_transformed[:, i_dim] = transform(
+                samples[:, i_dim], t, bounds=bounds[:, i_dim]
+            )
         sdf_values = self.microtile(queries_transformed)
-        # self.plot_transformed_untransformed(queries, queries_transformed)
+
         return sdf_values
 
     def _sanity_check(self):
@@ -238,10 +244,10 @@ def constantLatvec(value):
 
 
 def transform(x, t, bounds=[0, 1]):
-    # transform x from [0,1] to [0,1]
+    # transform x from [bounds[0], bounds[1]] to [-1,1]
     x_norm = (x - bounds[0]) / (bounds[1] - bounds[0])
     x_transformed = 2 * _torch.abs(t * x_norm / 2 - _torch.floor((t * x_norm + 1) / 2))
-    return x_transformed
+    return 2 * x_transformed - 1
 
 
 def check_tiling_input(tiling):