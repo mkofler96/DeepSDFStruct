069e0271a94641373fe7c7c8601f22d516de25ba
mkofler96 <mkofler96@users.noreply.github.com>
2026-07-16 07:37:02 +0000

    deploy: b86bba507e44fa89f841390b30b8b07983203d4e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.7.3.dev4+g86547ef44.d20260715',
+    VERSION: '1.7.3.dev5+gb86bba507',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
